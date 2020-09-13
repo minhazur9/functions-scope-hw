09c001b68d8aa49b743a07d282e7dc793558b8bd
min <minhazur9@gmail.com>
2020-09-13 16:47:14 -0400

    Commit 3 - Digit Sum

--- scripts/app.js
+++ scripts/app.js
@@ -7,18 +7,27 @@
 // 2. A return only passes an output, usually to another function or variable. Console.log just prints out to the console.
 // 3. The function being able to return a value gives it the abilty to pass its outputted value as an argument to another function.
 
-// Palindrome again.
+// Palindrome again
 
 function checkPalindrome(word){
     reverse = word.split('').reverse().join('');
-    if(word === reverse) {
-        return true;
-    }
-    else{
-        return false;
-    }
+    if(word === reverse) return true;
+    else return false;
+
 }
 
 console.log(checkPalindrome('radar'));
 console.log(checkPalindrome('borscht'));
 
+// Digit Sum
+
+function sumDigits(num){
+    const numArr = num.toString().split('');
+    let sum = 0;
+    for(let i = 0; i < numArr.length; i++){
+        sum += Number(numArr[i]);
+    }
+    return sum;
+}
+
+console.log(sumDigits(42));
